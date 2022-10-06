5c6c86949ab370643e6edabe22c9d906235a5b71
VinayKale1998 <vinaykale1999@gmail.com>
2022-10-06 12:06:22 +0530

    added a function for mutiplying

--- demo.js
+++ demo.js
@@ -4,3 +4,11 @@ function addTwoNumbers(a,b)
 }
 console.log(addTwoNumbers(2,3));
 console.log(addTwoNumbers(4,6));
+console.log(mutiplyTwoNumber(2,3));
+
+function mutiplyTwoNumber(c,d)
+{
+    return c*d;
+}
+console.log(mutiplyTwoNumber(2,3));
+
