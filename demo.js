db1c974467ae0e85649a0058045f332fd51e841d
Vinay Kale <vinaykale1999@gmail.com>
2022-10-26 15:24:27 +0530

    subtract

--- demo.js
+++ demo.js
@@ -6,14 +6,15 @@ console.log(addTwoNumbers(2,3));
 console.log(addTwoNumbers(4,6));
 console.log(mutiplyTwoNumber(2,3));
 
-function mutiplyTwoNumber(c,d)
+function mutiplyTwoNumber(c,d)f
 {
     return c*d;
 }
 console.log(mutiplyTwoNumber(2,3));
 //fssfs
 
-console.log(addTwoNumber(4,5));
-console.log("dummy");
+console.log(addTwoNumbers(4,5));
+console.log("dummy");f
 console.log("a");
 console.log("b");
+
