f94efaf09e4672d1a04c03831e60f4ffe6f95581
Vinay Kale <vinaykale1999@gmail.com>
2022-10-27 23:15:25 +0530

    area

---
+++ areaOfCircle.js
@@ -0,0 +1,7 @@
+let radius = 5;
+const pi = 22/7
+/*we can change the radius with the same identifier in 
+the upcoming lines but not the value of pi*/
+
+let area = (radius*pi^2)
+console.log(area)
