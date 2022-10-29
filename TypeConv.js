d5f1ec19b2c769bcfe57cc4e7cc492c3a762aac2
Vinay Kale <vinaykale1999@gmail.com>
2022-10-29 22:31:33 +0530

    TypeConv

---
+++ TypeConv.js
@@ -0,0 +1,13 @@
+let randomint = 1299;
+console.log(randomint,typeof randomint)
+
+let convertedint = String(randomint)
+console.log(convertedint, typeof convertedint)
+
+let str = "reconnaissance";
+let numint = Number(str)
+console.log(numint, typeof numint)
+let num = 4;
+num= Boolean(num); 
+Console.log(num, typeof num) //True and boolean
+// all numbers are true except 0 , but, if you use "  ! " while converting(num = !num), 0 will become true and all other numbers become false
