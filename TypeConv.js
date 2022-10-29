d04102fe488b94c648773dd18e653226d3c38b74
Vinay Kale <vinaykale1999@gmail.com>
2022-10-29 22:42:49 +0530

    Parse

--- TypeConv.js
+++ TypeConv.js
@@ -7,7 +7,17 @@ console.log(convertedint, typeof convertedint)
 let str = "reconnaissance";
 let numint = Number(str)
 console.log(numint, typeof numint)
+
+
 let num = 4;
 num= Boolean(num); 
-Console.log(num, typeof num) //True and boolean
-// all numbers are true except 0 , but, if you use "  ! " while converting(num = !num), 0 will become true and all other numbers become false
+console.log(num, typeof num);//True and boolean
+/* all numbers are true except 0 , but, if you use "  ! " while converting(num = !num)
+will become true and all other numbers become false*/
+
+
+
+let x= parseInt("112 Vinay");
+console.log(x); //112
+/* converting using Number typecast operator will give NaN, that's why parseInt
+was used, and even this will fail if the string starts with a non number character */
