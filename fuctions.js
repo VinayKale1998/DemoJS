da7a3c37bf302a08044b98533e06a8f774dea6dc
VinaKale1998 <vinaykale1999@gmail.com>
2022-11-09 23:21:26 +0530

    prime

--- fuctions.js
+++ fuctions.js
@@ -12,3 +12,28 @@ else
 
 const newLocal = 1989
 console.log(oddEven(newLocal))
+
+function prime(a)
+{
+    let m=0;
+for(b=2;b<=a/2;b++)
+
+{
+if(a%b==0)
+{
+   m++; 
+
+}
+
+}
+if(m>0)
+{
+    console.log(a+" is not a prime number")
+
+}
+else{
+    console.log(a+" is  a prime number")
+}
+}
+
+console.log(prime(7))
