ae29f25da408d6f0da1049acfc3303dbfa01c86e
VinaKale1998 <vinaykale1999@gmail.com>
2022-11-19 13:00:28 +0530

    loops

--- multi.js
+++ multi.js
@@ -10,4 +10,24 @@ let in3=[1,2,3,4]
 in3[3]=5;
 console.log(in3)
 
+//iteration
+let data=[1,3,4]
+data.forEach(e => {
+    console.log(e)})
+
+    //iteratin over a multidimensional array   using for each
+tot1.forEach(outer=>{
+    outer.forEach(inner => {console.log(inner)})});
+
+
+    //You can also use the for...of loop to iterate over the multidimensional array. For example,
+
+    for(let i of tot1){
+        for(let j of i)
+        {
+            console.log(j)
+        }
+    }
+
+
 
